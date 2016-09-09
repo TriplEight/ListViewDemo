ecfd5ecb62743e43921966ca52ea4d4ab01c1712
Denis Pisarev <pisarevden@gmail.com>
2016-09-09 12:36:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/listviewdemo/ListViewActivity1.java
@@ -0,0 +1,36 @@
+package com.example.android.listviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListViewActivity1 extends AppCompatActivity implements OnItemClickListener{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        ListView listView = (ListView) findViewById(R.id.sports_list);
+        listView.setOnItemClickListener(this);
+    }
+
+    /*
+     * Parameters:
+        adapter - The AdapterView where the click happened.
+        view - The view within the AdapterView that was clicked
+        position - The position of the view in the adapter.
+        id - The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+        Toast.makeText(this,((TextView) view).getText().toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+}
